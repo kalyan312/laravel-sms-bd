9dc45477b25e25b4f78961b2a4b8d90bafc41108
rrahul <rahulbgc21@gmail.com>
2021-11-01 13:27:10 +0600

    fix sms sending time

--- src/helpers.php
+++ src/helpers.php
@@ -12,3 +12,14 @@ if (!function_exists('sms')) {
         return new \Khbd\LaravelSmsBD\SMS();
     }
 }
+
+if (!function_exists('getCurrentDateTime')) {
+
+    function getCurrentDateTime()
+    {
+        if(class_exists(\Carbon\Carbon::class)){
+            return \Carbon\Carbon::now();
+        }
+        return date('Y-m-d H:i:s');
+    }
+}
